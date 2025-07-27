46e6c1c1ed0428f96573a63893a2a18ce8400f9d
KKW <97009196+Attraaa@users.noreply.github.com>
2025-07-27 22:27:11 +0900

    real first commit

---
+++ bin/main/static/script/index.js
@@ -0,0 +1,23 @@
+const menu = document.querySelector(".menu_ul");
+const sub_menu = document.querySelector(".sub_menu");
+const dim = document.querySelector(".dim");
+
+const profile = document.querySelector(".profile");
+const profile_menu = document.querySelector(".profile_menu");
+
+menu.addEventListener("mouseover", ()=> {
+    sub_menu.classList.add("show");  
+    dim.classList.add("show");  
+});
+menu.addEventListener("mouseout", ()=> {
+    sub_menu.classList.remove("show");
+    dim.classList.remove("show");
+});
+
+profile.addEventListener("mouseover", ()=> {
+    profile_menu.classList.add("show");  
+});
+profile.addEventListener("mouseout", ()=> {
+    profile_menu.classList.remove("show");  
+
+});
---
+++ src/main/java/net/aisw/FCfansite.java
@@ -0,0 +1,13 @@
+package net.aisw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FCfansite {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FCfansite.class, args);
+	}
+
+}
