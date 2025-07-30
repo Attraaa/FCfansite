1dc9d712bfbd6d527f68b46fc88d1abbcf5770eb
dmsdn2003 <73451482+dmsdn2003@users.noreply.github.com>
2025-07-30 23:59:56 +0900

    메인컨트롤, 팬사이트.java

--- src/main/java/net/aisw/MainController.java
+++ src/main/java/net/aisw/MainController.java
@@ -1,12 +1,11 @@
 package net.aisw;
 
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.time.LocalDate;
 import java.util.HashMap;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,10 +13,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import net.aisw.dto.HistoryDTO;
 import net.aisw.dto.userDTO;
+import net.aisw.service.HistoryService;
 import net.aisw.service.UserService;
 
 
@@ -28,15 +27,32 @@ public class MainController {
     private final FCfansite FCfansite;
 	@Autowired
 	UserService usv;
+	
+	@Autowired
+	private HistoryService hService;
 
     MainController(FCfansite FCfansite) {
         this.FCfansite = FCfansite;
     }
 	
 	@RequestMapping("/index")
-	public String StartPage() {
-		return "index";
+	public String StartPage(Model model) {
+		HistoryDTO history = hService.getHistory();
+		if (history != null) {
+			model.addAttribute("h_title", history.getH_title());
+			model.addAttribute("h_content", history.getH_content());
+			model.addAttribute("h_photo", history.getH_photo());
+			model.addAttribute("h_date", history.getH_date());
+			
+			LocalDate today = LocalDate.now();
+	        LocalDate historyDate = history.getH_date().toLocalDate(); // java.sql.Date → LocalDate
+	        int diffYear = today.getYear() - historyDate.getYear();
+
+	        model.addAttribute("diffYear", diffYear);
+		}
+		    return "index";
 	}
+	
 	@RequestMapping("/logout")
 	public String Logout(HttpSession session) {
 		session.invalidate();
@@ -48,6 +64,7 @@ public class MainController {
 	public String LoginPage() {
 		return "/user/login";
 	}
+	
 	@RequestMapping("/register")
 	public String RegisterPage() {
 		return "/user/register";
@@ -55,27 +72,19 @@ public class MainController {
 	@RequestMapping("/mypage")
 	public String MypagePage() {
 		return "/user/mypage";
-	}	
+	}
+	
 	
 	@RequestMapping("/login/response")
-	public String Login_ResponsePage(@ModelAttribute userDTO userVo, HttpSession session, HttpServletResponse response) throws IOException {
-
+	public String Login_ResponsePage(@ModelAttribute userDTO userVo, HttpSession session) {
+		
 		int log = usv.getLogin(userVo);
 		System.out.println(log);
 		if(log == 1) {	
-			String userName = usv.getUserName(userVo.getU_id());
 			session.setAttribute("userID", userVo.getU_id());
-			session.setAttribute("userName", userName);
+			return "redirect:/index";
 		}
-		else {
-			response.setCharacterEncoding("UTF-8");
-			response.setContentType("text/html; charset=UTF-8");
-			PrintWriter out = response.getWriter();
-			out.println("<script> alert('아이디 또는 비밀번호가 틀립니다.');");
-			out.println("history.go(-1); </script>");
-			out.close();
-		}
-		return "redirect:/index";
+		return "redirect:/login";
 	}
 	
 	@RequestMapping("/register/response")
@@ -91,9 +100,9 @@ public class MainController {
 	@RequestMapping("/mypage/response")
 	public String MypagePage_Response(@ModelAttribute userDTO userVo, HttpSession session) {
 		String userID = (String) session.getAttribute("userID");
-		String userJoinDate = (String) session.getAttribute("userJoinDate");
+		String userName = (String) session.getAttribute("userName");
 		
-		if(userJoinDate == null) {
+		if(userName == null) {
 			System.out.println("세션 생성");
 			HashMap<String, String> userInfo = usv.getUserInfo(userID);
 			System.out.println(userInfo);
@@ -102,13 +111,8 @@ public class MainController {
 			session.setAttribute("userJoinDate", userInfo.get("u_joinDate"));
 			session.setAttribute("userWhy", userInfo.get("u_why"));
 		}
-		return "redirect:/mypage";
-	}
-	@RequestMapping("/replacePassword/response")
-	public String ReplacePassword_Response(@ModelAttribute userDTO userVo) {
 		
-		boolean ReplacePwd = usv.ReplacePassword(userVo);
-		System.out.println(ReplacePwd);
+		
 		return "redirect:/mypage";
 	}
 	
